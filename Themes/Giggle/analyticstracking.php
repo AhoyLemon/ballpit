dddce12701347834a872eb8d7d7921690f38e85e
Lemon <lemon@hirelemon.com>
2016-11-30 17:08:00 -0600

    simultaneous Piwik.

--- Themes/Giggle/analyticstracking.php
+++ Themes/Giggle/analyticstracking.php
@@ -1,9 +1,29 @@
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+<?php // PIWIK ?>
+  <script type="text/javascript">
+    var _paq = _paq || [];
+    _paq.push(["setDomains", ["*.ballp.it"]]);
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u="//thefpl.us/analytics/";
+      _paq.push(['setTrackerUrl', u+'pwk.php']);
+      _paq.push(['setSiteId', '2']);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'pwk.js'; s.parentNode.insertBefore(g,s);
+    })();
+  </script>
+  <noscript><p><img src="//thefpl.us/analytics/pwk.php?idsite=2" style="border:0;" alt="" /></p></noscript>
 
-  ga('create', 'UA-15129990-11', 'auto');
-  ga('send', 'pageview');
-</script>
+
+<?php // Google Analytics ?>
+
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-15129990-11', 'auto');
+    ga('send', 'pageview');
+  </script>
+
