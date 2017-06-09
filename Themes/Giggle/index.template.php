e0788c02c8e82d355cd1ee5e947f4072e8518bc2
Lemon <AhoyLemon@users.noreply.github.com>
2017-06-08 22:53:01 -0500

    First email sent.

--- Themes/Giggle/index.template.php
+++ Themes/Giggle/index.template.php
@@ -102,7 +102,7 @@ function template_html_above()
 
   echo '
     <meta charset="iso-8859-1">
-    <link rel="stylesheet" type="text/css" href="', $settings['theme_url'], '/css/ballpit.css?lastUpdated=06.01.2017" />';
+    <link rel="stylesheet" type="text/css" href="', $settings['theme_url'], '/css/ballpit.css?lastUpdated=06.08.2017" />';
 
   echo '
     <!-- FAVICON -->
