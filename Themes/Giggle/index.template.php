76bcb12ca84e1b7f7e050ebe1519558a77f498b6
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2019-01-21 16:36:38 -0600

    Blockquote syntax

--- Themes/Giggle/index.template.php
+++ Themes/Giggle/index.template.php
@@ -102,7 +102,7 @@ function template_html_above()
 
   echo '
     <meta charset="iso-8859-1">
-    <link rel="stylesheet" type="text/css" href="', $settings['theme_url'], '/css/ballpit.css?lastUpdated=2019-01-11" />';
+    <link rel="stylesheet" type="text/css" href="', $settings['theme_url'], '/css/ballpit.css?lastUpdated=2019-01-21" />';
 
   echo '
     <!-- FAVICON -->
